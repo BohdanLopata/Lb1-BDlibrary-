42eb8ac8f0f0f27d9705c9f6dc828b7822c7b278
BohdanLopata <bohdan.lopata04@knu.ua>
2023-02-27 21:28:41 +0200

    Create Service.cs

---
+++ Models/Service.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Models;
+
+public partial class Service
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime Deadline { get; set; }
+
+    public string? Information { get; set; }
+
+    public int WorkerId { get; set; }
+
+    public int CarServiceId { get; set; }
+
+    public int CarId { get; set; }
+
+    public decimal Price { get; set; }
+
+    public virtual Car Car { get; set; } = null!;
+
+    public virtual CarService CarService { get; set; } = null!;
+
+    public virtual Worker Worker { get; set; } = null!;
+}
