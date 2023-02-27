e543ca65c49ce79251767b8f3cad034b9ecd5ba5
BohdanLopata <bohdan.lopata04@knu.ua>
2023-02-27 21:28:24 +0200

    Create Car.cs

---
+++ Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Models;
+
+public partial class Car
+{
+    public int Id { get; set; }
+
+    public string Breakage { get; set; } = null!;
+
+    public string? Information { get; set; }
+
+    public int OwnerId { get; set; }
+
+    public int BrandId { get; set; }
+
+    public virtual Brand Brand { get; set; } = null!;
+
+    public virtual Owner Owner { get; set; } = null!;
+
+    public virtual ICollection<Service> Services { get; } = new List<Service>();
+}
