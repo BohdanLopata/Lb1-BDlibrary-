a0187e202f8d42c135aecbdf7fafafcf8dda3b34
BohdanLopata <bohdan.lopata04@knu.ua>
2023-02-27 21:24:36 +0200

    Create Brand.cs

---
+++ Models/Brand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Models;
+
+public partial class Brand
+{
+    public int Id { get; set; }
+
+    public string? Model { get; set; }
+
+    public string BrandName { get; set; } = null!;
+
+    public virtual ICollection<Car> Cars { get; } = new List<Car>();
+}
