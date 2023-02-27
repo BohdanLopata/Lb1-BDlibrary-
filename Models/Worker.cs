26c39d0c9cee925eb0a72dac0dd734eff0671ece
BohdanLopata <bohdan.lopata04@knu.ua>
2023-02-27 21:28:43 +0200

    Create Worker.cs

---
+++ Models/Worker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Models;
+
+public partial class Worker
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public int WorkExperience { get; set; }
+
+    public string Information { get; set; } = null!;
+
+    public int CarServiceId { get; set; }
+
+    public virtual CarService CarService { get; set; } = null!;
+
+    public virtual ICollection<Service> Services { get; } = new List<Service>();
+}
