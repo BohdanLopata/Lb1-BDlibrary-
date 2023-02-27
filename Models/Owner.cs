3fcbe8c6c2c2708f5242ea9adebf9cf577614ce9
BohdanLopata <bohdan.lopata04@knu.ua>
2023-02-27 21:28:39 +0200

    Create Owner.cs

---
+++ Models/Owner.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Models;
+
+public partial class Owner
+{
+    public int Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? MiddleName { get; set; }
+
+    public string LastName { get; set; } = null!;
+
+    public string Number { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public virtual ICollection<Car> Cars { get; } = new List<Car>();
+}
