1ee8c43cd83158907c9a8e6868774207b480f2ac
BohdanLopata <bohdan.lopata04@knu.ua>
2023-02-27 21:28:32 +0200

    Create City.cs

---
+++ Models/City.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LibraryWebApplication.Models;
+
+public partial class City
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public virtual ICollection<CarService> CarServices { get; } = new List<CarService>();
+}
